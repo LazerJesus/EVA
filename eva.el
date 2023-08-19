517fb2f92ccfed14c54a6d749800a0f2c7e48533
finn <finn@cofenster.com>
2023-08-19 17:35:14 +0200

    Init

---
+++ eva.el
@@ -0,0 +1,16 @@
+;;; eva.el --- Emacs virtual assistant -*- lexical-binding: t; -*-
+
+;; Author: Finn Luca Frotscher <finn@finnfrotscher.com>
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.5"))
+;; Keywords: convenience
+;; URL: https://github.com/LazerJesus/EVA
+
+;; This file is not part of GNU Emacs.
+
+;; Commentary:
+;; Brief introduction to what your package does and/or why it exists.
+
+;; Code:
+
+
